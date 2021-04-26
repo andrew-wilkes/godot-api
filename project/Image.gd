e3ffd70099e5419f52a126d80f8a2888c3fe72f4
andrew-wilkes <github@urgentclick.com>
2021-04-26 18:30:31 +0900

    Add image code

--- project/Image.gd
+++ project/Image.gd
@@ -1,11 +1,26 @@
 extends Node2D
 
 func _ready():
-	# Assign colors
-	var num_sprites = $Sprites.get_child_count()
-	var hue_step = 1.0 / num_sprites
+	generate_image(Vector2(32, 32), Color.white, "white.png")
+	generate_image(Vector2(128, 128), Color(0, 0, 0, 0), "bg.png")
+	
 	var hue = 0.0
-	for s in $Sprites.get_children():
-		var sprite : Sprite = s
-		sprite.modulate = Color.from_hsv(hue, 0.5, 1.0)
+	var hue_step = 1.0 / $BG/Sprites.get_child_count()
+	var img = $BG.texture.get_data()
+	for sp in $BG/Sprites.get_children():
+		var sprite : Sprite = sp
+		var sqr = sprite.texture.get_data()
+		var mod = Color.from_hsv(hue, 0.5, 1.0)
+		sprite.modulate = mod
+		sqr.fill(mod)
+		img.blend_rect(sqr, Rect2(Vector2(0,0), sqr.get_size()), sp.position)
 		hue += hue_step
+	img.save_png("composite.png")
+
+
+func generate_image(size: Vector2, color: Color, fn: String):
+	var img = Image.new()
+	img.create(size.x, size.y, false, Image.FORMAT_RGBA8)
+	img.fill(color)
+	img.save_png(fn)
+	
